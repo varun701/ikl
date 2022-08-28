d8bb20526949132836205fc78fe9c65b03a31083
Varun <56062072+varun701@users.noreply.github.com>
2022-08-28 09:42:10 +0530

    Update verify command

--- commands/verify.js
+++ commands/verify.js
@@ -1,10 +1,17 @@
-import { PermissionFlagsBits, SlashCommandBuilder } from 'discord.js'
+import { EmbedBuilder, PermissionFlagsBits, SlashCommandBuilder } from 'discord.js'
 
 const data = {
   name: 'verify',
 }
 
 async function execute(interaction) {
+  // MARK: Remove this and support config at start up
+  config.dCode = {
+    NEW_COUPLE_LOG_MSG: 'New couple have arrived!!',
+    NEW_VERIFIED_MEMBER_LOG_MSG: 'Welcome to verified only section of server!',
+    NEW_VERIFIED_PLUS_MEMBER_LOG_MSG: 'Congratulations! You are now verified plus member!',
+  }
+
   const subCommand = interaction.options.getSubcommand()
   const awardRoleID =
     subCommand === 'couple'
@@ -13,20 +20,60 @@ async function execute(interaction) {
         ? interaction.client.KeyVC.get('ROLE_VERIFIED')
         : interaction.client.KeyVC.get('ROLE_VERIFIED_PLUS')
 
+  let messageContent =
+    subCommand === 'couple'
+      ? config.dCode.NEW_COUPLE_LOG_MSG
+      : subCommand === 'member'
+        ? config.dCode.NEW_VERIFIED_MEMBER_LOG_MSG
+        : config.dCode.NEW_VERIFIED_PLUS_MEMBER_LOG_MSG
+
   const targetUser = await interaction.options.getUser('member')
   const targetMember = await interaction.guild.members.fetch(targetUser)
   await targetMember.roles.add(awardRoleID)
+  const targetMemberEmbed = generateSimpleInfo(targetMember, interaction.user)
+  const embedsToSend = [targetMemberEmbed]
+  messageContent = messageContent.concat(`  <@${targetUser.id}>`)
 
   if (subCommand === 'couple') {
     const targetUserPartner = await interaction.options.getUser('partner')
     const targetMemberPartner = await interaction.guild.members.fetch(targetUserPartner)
     await targetMemberPartner.roles.add(awardRoleID)
+    const targetMemberPartnerEmbed = generateSimpleInfo(targetMemberPartner, interaction.user)
+    embedsToSend.push(targetMemberPartnerEmbed)
+    messageContent = messageContent.concat(`  <@${targetUserPartner.id}>`)
   }
+
+  const generalLogChannelID =
+    subCommand === 'couple'
+      ? interaction.client.KeyVC.get('CHANNEL_VERIFICATION_LOGS_COUPLE')
+      : subCommand === 'member'
+        ? interaction.client.KeyVC.get('CHANNEL_VERIFICATION_LOGS_MEMBER')
+        : interaction.client.KeyVC.get('CHANNEL_VERIFICATION_LOGS_PLUS')
+  const generalLogChannel = await interaction.client.channels.fetch(generalLogChannelID)
+  await generalLogChannel.send({
+    content: messageContent,
+    embeds: embedsToSend,
+  })
+
+  const modLogChannelID = interaction.client.KeyVC.get('CHANNEL_VERIFICATION_LOGS_MODS')
+  const modLogChannel = await interaction.client.channels.fetch(modLogChannelID)
+  await modLogChannel.send({
+    content: subCommand,
+    embeds: embedsToSend,
+  })
+
   await interaction.reply({
     content: 'Done',
     ephemeral: true,
   })
   return
+}
+
+function generateSimpleInfo(target, user) {
+  return new EmbedBuilder()
+    .setTitle(target.displayName)
+    .setDescription(`${target.user.tag}\n\n**Moderator**\n<@${user.id}>`)
+    .setThumbnail(target.displayAvatarURL())
 }
 
 async function preExecute(_client) {
