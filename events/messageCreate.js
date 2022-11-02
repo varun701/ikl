9819ed8224886fdae095e05c28ffb3d4ba5118cb
Varun <56062072+varun701@users.noreply.github.com>
2022-11-02 18:40:33 +0530

    .

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,5 +1,6 @@
 async function execute(message) {
-  if (message.type == 'APPLICATION_COMMAND' && message.channel.id == '960123765583392788' && message.interaction.commandName == 'bump') {
+  if (message.type == 'APPLICATION_COMMAND' && message.interaction.commandName == 'bump') {
+    console.log('server bump')
     message.channel.permissionOverwrites.edit(message.guild.roles.everyone, {
       VIEW_CHANNEL: false,
     })
