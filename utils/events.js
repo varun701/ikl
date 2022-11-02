fc8eef6539db73e1516d56ff70d0864411d6aba9
Varun <56062072+varun701@users.noreply.github.com>
2022-11-02 14:07:34 +0530

    .

--- utils/events.js
+++ utils/events.js
@@ -1,4 +1,7 @@
 import ready from '../events/ready.js'
+import debug from '../events/debug.js'
+import error from '../events/errror.js'
+import messageCreate from '../events/messageCreate.js'
 import interactionCreate from '../events/interactionCreate.js'
 
-export const eventsList = [ready, interactionCreate]
+export const eventsList = [ready, debug, error, interactionCreate, messageCreate]
