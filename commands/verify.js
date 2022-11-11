b17723500ca574e6d2bb0b99b4c53fdb98363b73
Varun <56062072+varun701@users.noreply.github.com>
2022-11-11 21:32:50 +0530

    feat: command: 'verify'

---
+++ commands/verify.js
@@ -0,0 +1,77 @@
+// eslint-disable-next-line no-unused-vars
+import { SlashCommandBuilder, ChatInputCommandInteraction, GuildMember } from 'discord.js'
+import { getAssets, verifyAssets } from '../utils/assets.js'
+
+const data = {
+  name: 'verify',
+}
+
+const builder = new SlashCommandBuilder()
+  .setName('verify')
+  .setDescription('Verify members')
+  .setDMPermission(false)
+  .addSubcommand((subcommand) =>
+    subcommand
+      .setName('member')
+      .setDescription('Give \'Verified\' role')
+      .addUserOption((option) => option.setName('member').setDescription('The member').setRequired(true)),
+  )
+  .addSubcommand((subcommand) =>
+    subcommand
+      .setName('plus')
+      .setDescription('Give \'Verified Plus\' role')
+      .addUserOption((option) => option.setName('member').setDescription('The member').setRequired(true)),
+  )
+  .addSubcommand((subcommand) =>
+    subcommand
+      .setName('couple')
+      .setDescription('Give \'Couple\' role')
+      .addUserOption((option) => option.setName('member').setDescription('The member').setRequired(true))
+      .addUserOption((option) => option.setName('partner').setDescription('The partner').setRequired(true)),
+  )
+
+/**
+ * @param {GuildMember} member
+ */
+const verifyAndLog = async (member, moderator, type) => {
+  const message = {
+    content: verifyAssets(`message_content_${type}`, { id: member.id }),
+    embeds: [{
+      title: member.displayName,
+      description: member.user.tag,
+      color: 3092790,
+      thumbnail: {
+        url: member.displayAvatarURL(),
+      },
+      fields: [{ name: 'Moderator', value: `<@${moderator.id}>` }],
+    }],
+  }
+  await member.roles.add(keyv.get(`role_verified_${type}`))
+  const channel = await member.client.channels.fetch(keyv.get(`channel_verified_${type}`))
+  await channel?.send(message)
+  const logChannel = await member.client.channels.fetch(keyv.get('channel_logs_verified'))
+  await logChannel?.send(message)
+}
+
+/**
+ * @param {ChatInputCommandInteraction} interaction
+ */
+const execute = async (interaction) => {
+  await interaction.deferReply({ ephemeral: true })
+
+  const type = interaction.options.getSubcommand()
+  const member = interaction.options.getMember('member')
+  const partner = interaction.options.getMember('partner')
+
+  await verifyAndLog(member, interaction.member, type)
+  if (partner !== null && partner !== undefined) {
+    await verifyAndLog(partner, interaction.member, type)
+  }
+  await interaction.editReply(getAssets('success'))
+}
+
+export default {
+  data,
+  builder,
+  execute,
+}
--- utils/commands.js
+++ utils/commands.js
@@ -1,4 +1,5 @@
 import viewIntro from '../commands/viewIntro.js'
 import notice from '../commands/notice.js'
+import verify from '../commands/verify.js'
 
-export const commandsList = [viewIntro, notice]
+export const commandsList = [viewIntro, notice, verify]
